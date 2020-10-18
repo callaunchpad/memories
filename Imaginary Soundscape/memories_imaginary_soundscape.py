0bd61cf95446975721ad54e3ddc055f009ff33ad
Rachel Lee <bokyounglee@berkeley.edu>
2020-10-18 14:25:07 -0700

    wrote to json

--- Imaginary Soundscape/memories_imaginary_soundscape.py
+++ Imaginary Soundscape/memories_imaginary_soundscape.py
@@ -12,6 +12,8 @@ from pathlib import Path
 import numpy as np
 from PIL import Image
 
+import json
+
 from soundnet import SoundNet
 import os
 
@@ -62,6 +64,8 @@ soundnet_model = SoundNet()
 soundnet_model.load_state_dict(torch.load("soundnet8_final.pth"))
 
 sound_dataset_path = Path.cwd().joinpath('data/sound_samples')
+
+#practice sound sample input
 sample_loaded, rate = librosa.load(sound_dataset_path.joinpath('9447523993_9_9_3.mp3'))
 
 sample_loaded = np.reshape(sample_loaded, (1, 1, sample_loaded.shape[0], 1))
@@ -75,3 +79,30 @@ pred = pred[0].detach().numpy()
 pred = pred[:,0][:,0]
 print(pred)
 print(pred.size)
+
+# take the l2 distance of the image array and sort by how close they are
+# take the first three (closest)
+
+
+# Create JSON file with JSON object of sound_samples
+# key is the file path and the value is the 1000 length array
+sound_samples = {}
+
+for filename in os.listdir(sound_dataset_path):
+    
+    sample_loaded, rate = librosa.load(sound_dataset_path.joinpath(filename))
+    if len(sample_loaded) > 230000:
+        sample_loaded = np.reshape(sample_loaded, (1, 1, sample_loaded.shape[0], 1))
+        #convert to tensor
+        sample_loaded = torch.tensor(sample_loaded)
+        sample_loaded.to(device)
+        pred = soundnet_model(torch.tensor(sample_loaded))
+        pred = pred[0].detach().numpy()
+        pred = pred[:,0][:,0]
+
+
+        #store 1000 length array as value in dictionary
+        sound_samples[filename] = pred.tolist()
+
+with open('sound_samples.json', 'w') as outfile:
+    json.dump(sound_samples, outfile, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
