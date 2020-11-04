6eff57472d80c357194aabd6f3c32555c9733cce
James Jiao <james.j.jiao@gmail.com>
2020-11-03 17:23:33 -0800

    add blog

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 // pages for this product
 import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
+import BlogPage from "views/BlogPage/BlogPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
 
@@ -17,6 +18,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/aboutus" component={LandingPage} />
+      <Route path="/blog" component={BlogPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/uploadimage" component={LoginPage} />
       <Route path="/" component={Components} />
