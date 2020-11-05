b17c74cc705e25c45e02fef1e5b926086a87787c
James Jiao <james.j.jiao@gmail.com>
2020-11-04 16:59:19 -0800

    updated about us

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -33,36 +33,24 @@ export default function Footer(props) {
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/presentation?ref=mkr-footer"
+                href="/aboutus"
                 className={classes.block}
-                target="_blank"
               >
                 About us
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="http://blog.creative-tim.com/?ref=mkr-footer"
+                href="/blog"
                 className={classes.block}
-                target="_blank"
               >
                 Blog
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                href="https://www.creative-tim.com/license?ref=mkr-footer"
-                className={classes.block}
-                target="_blank"
-              >
-                Licenses
               </a>
             </ListItem>
           </List>
         </div>
         <div className={classes.right}>
-          &copy; {1900 + new Date().getYear()} , made with{" "}
-          <Favorite className={classes.icon} /> {" "}
+          &copy; {1900 + new Date().getYear()}, Memories
         </div>
       </div>
     </footer>
